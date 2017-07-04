7ba2ece8bb743bfa08622b1f4dd751556599f24c
Rafael Dohms <rdohms@gmail.com>
2017-07-04 23:03:57 +0200

    Cleanup

--- radar_data.js
+++ radar_data.js
@@ -42,21 +42,72 @@ var radar_data = [
     "left" : 45,
     "top" : 188,
     "color" : "#8FA227",
-    "items" : 0
+    "items" : [
+  {
+    "name": "RFC3339",
+    "pc": {
+      "r": 99,
+      "t": 152
+    },
+    "movement": "c"
+  },
+  {
+    "name": "Symfony",
+    "pc": {
+      "r": 67,
+      "t": 100
+    },
+    "movement": "c"
+  },
+  {
+    "name": "Timestamps",
+    "pc": {
+      "r": 247,
+      "t": 126
+    },
+    "movement": "c"
+  }
+]
   },
   { 
     "quadrant": "Data Management",
     "left": w-300+30,
     "top" : 188,
     "color" : "#587486",
-    "items" : 0
+    "items" : [
+  {
+    "name": "MongoDB",
+    "pc": {
+      "r": 65,
+      "t": 49
+    },
+    "movement": "c"
+  },
+  {
+    "name": "Postgres",
+    "pc": {
+      "r": 193,
+      "t": 8
+    },
+    "movement": "c"
+  }
+]
   },
   {   
     "quadrant": "Platforms & Infrastructure",
     "left" :45,
     "top" : (h/2 + 38),
     "color" : "#DC6F1D",
-    "items" : 0
+    "items" : [
+  {
+    "name": "Prometheus",
+    "pc": {
+      "r": 64,
+      "t": 255
+    },
+    "movement": "c"
+  }
+]
   },
   { 
     "quadrant": "Languages",
@@ -65,10 +116,19 @@ var radar_data = [
     "top" :   (h/2 + 38),
     "items" : [
   {
-    "name": "PHP",
+    "name": "PHP 7.*",
     "pc": {
-      "r": 115,
-      "t": 293
+      "r": 95,
+      "t": 345
+    },
+    "movement": "c",
+    "url": "http://www.php.net"
+  },
+  {
+    "name": "PHP 5.*",
+    "pc": {
+      "r": 257,
+      "t": 316
     },
     "movement": "c"
   }
